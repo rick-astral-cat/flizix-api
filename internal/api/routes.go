811ed37f5ab1c95550d2b6ac49c55fe71e44ef22
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2026-05-26 12:04:01 -0600

    feat: setup swagger and adaptions for endpoints

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,10 +1,19 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
 
-func (api *Config) RegisterRoutes(mux *http.ServeMux) {
+	_ "github.com/rick-astral-cat/flizix-api/docs"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+)
+
+func (api *Config) RegisterRoutes(mux *http.ServeMux, env string) {
 	mux.HandleFunc("POST /users", api.HandleCreateUser)
 	mux.HandleFunc("GET /health", api.HandleHealth)
+
+	if env == "development" {
+		mux.Handle("GET /swagger/", httpSwagger.Handler(httpSwagger.URL("/swagger/doc.json")))
+	}
 
 	// Private Routes
 	// profileHandler := http.HandlerFunc(api.HandleProfile)
--- internal/api/users.go
+++ internal/api/users.go
@@ -19,6 +19,31 @@ type CreateUserRequest struct {
 	PasskeyID string `json:"passkey_id"`
 }
 
+type UserResponse struct {
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email,omitempty"`
+}
+
+func MapUserToResponse(user db.User) UserResponse {
+	return UserResponse{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email.String,
+	}
+}
+
+// HandleCreateUser godoc
+// @Summary      Create new user
+// @Description  Register new user on DB with name, email and passkey ID.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        user  body      CreateUserRequest  true  "User data"
+// @Success      201   {object}	 UserResponse
+// @Failure      400   {string}  string  "Invalid JSON"
+// @Failure      500   {string}  string  "Internal Server Error"
+// @Router       /users [post]
 func (api *Config) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
 	var req CreateUserRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
@@ -48,7 +73,9 @@ func (api *Config) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	response := MapUserToResponse(user)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(response)
 }
