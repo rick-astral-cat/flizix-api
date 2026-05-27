fc5d8426e573e76d661fe8fbce37dab99a7db505
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2026-05-26 18:13:22 -0600

    feat: telegram registration endpoint and profile route added

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -10,12 +10,13 @@ import (
 func (api *Config) RegisterRoutes(mux *http.ServeMux, env string) {
 	mux.HandleFunc("POST /users", api.HandleCreateUser)
 	mux.HandleFunc("GET /health", api.HandleHealth)
+	mux.HandleFunc("POST /auth/telegram", api.HandleTelegramLogin)
 
 	if env == "development" {
 		mux.Handle("GET /swagger/", httpSwagger.Handler(httpSwagger.URL("/swagger/doc.json")))
 	}
 
 	// Private Routes
-	// profileHandler := http.HandlerFunc(api.HandleProfile)
-	// mux.Handle("GET /profile", api.JWTMiddleware(profileHandler))
+	profileHandler := http.HandlerFunc(api.HandleGetProfile)
+	mux.Handle("GET /me", api.JWTMiddleware(profileHandler))
 }
