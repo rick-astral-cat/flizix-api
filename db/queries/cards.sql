bb25212666dd99f2fe1c63e5aeb79a81c650ee7b
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2026-06-17 13:36:38 -0600

    feat: add cards tasks on database for sqlc

---
+++ db/queries/cards.sql
@@ -0,0 +1,32 @@
+-- name: CreateCard :one
+INSERT INTO cards (
+    name,
+    type,
+    credit_limit,
+    cutoff_date,
+    user_id
+) VALUES (?,?,?,?,?)
+RETURNING *;
+
+-- name: GetCardByID :one
+SELECT * FROM cards
+WHERE id = ? AND user_id = ? AND deleted_at IS NULL
+LIMIT 1;
+
+-- name: ListCardsByUser :many
+SELECT * FROM cards
+WHERE user_id = ? AND deleted_at IS NULL
+ORDER BY name ASC;
+
+-- name: UpdateCard :one
+UPDATE cards
+SET name = ?,
+    type = ?,
+    credit_limit = ?,
+    cutoff_date = ?
+WHERE id = ? AND user_id = ? AND deleted_at is NULL
+RETURNING *;
+
+-- name: SoftDeleteCard :exec
+UPDATE cards SET deleted_at = CURRENT_TIMESTAMP
+WHERE id = ? AND user_id = ?;
